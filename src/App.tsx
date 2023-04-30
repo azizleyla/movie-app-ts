3f38519377d42c662f177b2658dd3b0898259d20
Leyla Aziz <ezizovaleyla1@gmail.com>
2023-04-30 23:39:16 +0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import MovieList from "./components/MovieList";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/4/list/1?page=1&api_key=4faf715e4beccd69d65cf8aa0f91cc5a",
+      )
+      .then((res) => console.log(res.data.results));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList />
     </div>
   );
 }
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieList = () => {
+  return <div className="bg-red-500">MovieList</div>;
+};
+
+export default MovieList;
