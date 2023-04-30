05ddf6c7128bd45d8f39c106a08b6d165d0a6f53
Leyla Aziz <ezizovaleyla1@gmail.com>
2023-05-01 00:05:47 +0400

    steps

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,31 @@ import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import MovieList from "./components/MovieList";
-
+//https://image.tmdb.org/t/p/w500/3CxUndGhUcZdt1Zggjdb2HkLLQX.jpg
 function App() {
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/4/list/1?page=1&api_key=4faf715e4beccd69d65cf8aa0f91cc5a",
+        "https://api.themoviedb.org/3/discover/movie?api_key=4faf715e4beccd69d65cf8aa0f91cc5a",
       )
-      .then((res) => console.log(res.data.results));
-  }, []);
+      .then((res) => console.log(res.data));
+  });
   return (
     <div className="App">
-      <MovieList />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
---
+++ src/api/movieApi.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const movieApi = () => {
+  return <div>movieApi</div>;
+};
+
+export default movieApi;
